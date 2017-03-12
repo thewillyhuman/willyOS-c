b9b0297174406be2997d4b6304c808b69676daba
Guillermo Facundo Colunga <colunga91@gmail.com>
2017-03-12 12:44:23 +0100

    .

--- willyOS/examples/Properties/AssemblyInfo.cs
+++ willyOS/examples/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("")]
+[assembly: AssemblyCopyright("${AuthorCopyright}")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
