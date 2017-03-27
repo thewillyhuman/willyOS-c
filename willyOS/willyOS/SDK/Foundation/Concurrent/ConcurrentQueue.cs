3b6e443ca886c7c40b6b736415c1e8ce453ac46d
Guillermo Facundo Colunga <colunga91@gmail.com>
2017-03-28 00:18:58 +0200

    Consurrent queue added

---
+++ willyOS/willyOS/SDK/Foundation/Concurrent/ConcurrentQueue.cs
@@ -0,0 +1,74 @@
+﻿
+namespace Foundation.Concurrent {
+
+	/// <summary>
+	/// Concurrent queue.
+	/// </summary>
+	public class ConcurrentQueue<T> {
+
+		/// <summary>
+		/// The list.
+		/// </summary>
+		NSList<T> _list;
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="T:Foundation.Concurrent.ConcurrentQueue`1"/> class.
+		/// </summary>
+		public ConcurrentQueue() {
+			_list = new NSList<T>();
+		}
+
+		/// <summary>
+		/// Gets the number of elements.
+		/// </summary>
+		/// <value>The number of elements.</value>
+		public int NumberOfElements {
+			get {
+				lock(this)
+					return _list.Count;
+			}
+		}
+
+		/// <summary>
+		/// Gets a value indicating whether this <see cref="T:Foundation.Concurrent.ConcurrentQueue`1"/> is empty.
+		/// </summary>
+		/// <value><c>true</c> if is empty; otherwise, <c>false</c>.</value>
+		public bool IsEmpty {
+			get {
+				lock(this)
+					return _list.Count == 0;
+			}
+		}
+
+		/// <summary>
+		/// Enqueue the specified item.
+		/// </summary>
+		/// <returns>The enqueue.</returns>
+		/// <param name="item">Item.</param>
+		public void Enqueue(T item) {
+			lock(this)
+				_list.Insert(index: _list.Count-1, item: item);
+		}
+
+		/// <summary>
+		/// Dequeue this instance.
+		/// </summary>
+		/// <returns>The dequeue.</returns>
+		public T Dequeue() {
+			lock(this) {
+				var tmp = _list[0];
+				_list.RemoveAt(0);
+				return tmp;
+			}
+		}
+
+		/// <summary>
+		/// Peek this instance.
+		/// </summary>
+		/// <returns>The peek.</returns>
+		public T Peek() {
+			lock(this)
+				return _list[0];
+		}
+	}
+}
---
+++ willyOS/willyOSTests/SDKTests/FoundationTests/Concurrent/ConcurrentQueueTest.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Threading;
+using Foundation.Concurrent;
+using NUnit.Framework;
+
+namespace willyOS.tests {
+
+	/// <summary>
+	/// Concurrent queue test.
+	/// </summary>
+	public class ConcurrentQueueTest {
+
+		ConcurrentQueue<int> cqueue;
+
+		[SetUp]
+		public void BeforeEachTest() {
+			cqueue = new ConcurrentQueue<int>();
+		}
+
+		[Test]
+		public void Test() {
+			var ntest = 100;
+			Thread[] threads = new Thread[ntest];
+			for(int i = 0; i < ntest-5; i = i + 5) {
+				threads[i] = new Thread(Add);
+				threads[i+1] = new Thread(NumberOfElements);
+				threads[i+2] = new Thread(Peek);
+				//threads[i+3] = new Thread(Remove);
+				threads[i+4] = new Thread(IsEmpty);
+			}
+			for(int i = 0; i < threads.Length; i++) {
+				threads[i].Start();
+			}
+
+			foreach(var thread in threads) {
+				thread.Join();
+			}
+			Assert.AreEqual(true, true);
+			Console.Write("Lenght: {0}", cqueue.NumberOfElements);
+		}
+
+		internal void Add() {
+			cqueue.Enqueue(1);
+		}
+
+		internal void Remove() {
+			cqueue.Dequeue();
+		}
+
+		internal void NumberOfElements() {
+			var i = cqueue.NumberOfElements;
+		}
+
+		internal void IsEmpty() {
+			var i = cqueue.IsEmpty;
+		}
+
+		internal void Peek() {
+			cqueue.Peek();
+		}
+	}
+}
